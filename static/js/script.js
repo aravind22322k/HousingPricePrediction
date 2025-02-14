2d8e0b24e7a9ce12141ac92435c53b9d6dd90180
aravind22322k <aravindsg072@gmail.com>
2025-02-14 23:25:02 +0530

    Create script.js

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+// This JavaScript file will be loaded when the page loads
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript loaded successfully!");
+    // You can add more JavaScript functionality here if needed.
+});
